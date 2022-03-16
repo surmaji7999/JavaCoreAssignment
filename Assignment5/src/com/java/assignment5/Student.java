9ecd1578a8f8baa4446c59765bb72eb3e6492d34
Abhishek Kushwaha <surmaji7999@gmail.com>
2022-03-16 10:44:10 +0530

    Changed dateOfBirth type from java.lang.String to java.util.date with file output2

--- Assignment5/src/com/java/assignment5/Student.java
+++ Assignment5/src/com/java/assignment5/Student.java
@@ -1,6 +1,9 @@
 package com.java.assignment5;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Student implements Serializable{
     /**
@@ -8,11 +11,11 @@ public class Student implements Serializable{
      */
     private static final long serialVersionUID = 5581839595690640568L;
     String firstName;
-    String dateOfBirth;
+    Date dateOfBirth;
     Address address;
-    Student(String firstName,String dateOfBirth,String city, String state,int pinCode,String country){
+    Student(String firstName,String dateOfBirth,String city, String state,int pinCode,String country) throws ParseException{
         this.firstName=firstName;
-        this.dateOfBirth=dateOfBirth;
+        this.dateOfBirth= new SimpleDateFormat("dd/MM/yyyy").parse(dateOfBirth);
        address= new Address(city,state,pinCode,country);
     }
     @Override
